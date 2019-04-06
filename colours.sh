d0dd64d9600154b83bf7c472b5144dc680da2d84
Alexander Rodin <rodin.alexander@gmail.com>
2019-04-06 17:14:13 +0300

    Show background colors

--- colours.sh
+++ colours.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
 for i in {0..255}; do
-  if (((i + 1) % 8 == 0)); then
-    width=0
-  else
-    width=5
-  fi
-  printf "\x1b[38;5;${i}mcolour%-${width}d\x1b[0m" $i
+  printf "\x1b[48;5;${i}mcolour%-5d\x1b[0m" $i
   if (((i + 1) % 8 == 0 && i != 255)); then
     printf "\n"
   fi
