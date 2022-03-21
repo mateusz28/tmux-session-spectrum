013e1d31dd3794c4936efd48301984115762e47a
Mateusz <mateusz28@gmail.com>
2022-03-21 14:34:29 +0100

    fixe remote colors issue

--- tmux-session-spectrum.sh
+++ tmux-session-spectrum.sh
@@ -12,11 +12,33 @@ for map in "${ARRAY[@]}" ; do
       STYLE=$VALUE
     fi
 done
-tmux set -t $SESSION_NAME status-style bg=$STYLE
 tmux set -t $SESSION_NAME pane-active-border-style fg=$STYLE
-tmux set-hook -t $SESSION_NAME after-new-window \
+tmux set -t $SESSION_NAME status-style bg=$STYLE
+
+tmux set-hook -t $SESSION_NAME after-new-window[0] \
   "set -t $SESSION_NAME pane-active-border-style fg=$STYLE"
+tmux set-hook -t $SESSION_NAME after-new-window[1] \
+  "set -t $SESSION_NAME status-style bg=$STYLE"
 
 #tmux nested
-tmux bind -n M-F11 "set -q -t $SESSION_NAME status-bg colour0"
-tmux bind -n M-F12 "set -q -t $SESSION_NAME status-bg $STYLE"
+tmux set-hook -t $SESSION_NAME after-new-window[2]\
+  "bind -n M-F11 set -q -t $SESSION_NAME status-bg colour0"
+tmux set-hook -t $SESSION_NAME after-new-window[3]\
+  "bind -n M-F12 set -q -t $SESSION_NAME status-bg $STYLE"
+
+tmux bind -n M-F11 set -q -t $SESSION_NAME status-bg colour0
+tmux bind -n M-F12 set -q -t $SESSION_NAME status-bg $STYLE
+#tmux nested
+ARGS_ENTER="send-keys M-F11 \; set -q status-bg $STYLE \; set-option -g window-active-style fg=default,bg=colour0 \; bind-key -n M-h select-pane -L \; bind-key -n M-j select-pane -D \; bind-key -n M-k select-pane -U \; bind-key -n M-l select-pane -R \; set-option -gq prefix M-z"
+
+ARGS_EXIT="send-keys M-F12 \; set -q status-bg colour8 \; set-option -g window-active-style fg=default,bg=default \; unbind-key -n M-h \; unbind-key -n M-j \; unbind-key -n M-k \; unbind-key -n M-l \; set-option -gq prefix M-s"
+
+tmux bind -n M-C-j $ARGS_ENTER
+tmux bind -n M-C-k $ARGS_EXIT
+
+tmux set-hook -t $SESSION_NAME after-new-window[4]\
+  "bind -n M-C-j $ARGS_ENTER"
+tmux set-hook -t $SESSION_NAME after-new-window[5]\
+  "bind -n M-C-k $ARGS_EXIT"
+
+
--- tmux-session-spectrum.tmux
+++ tmux-session-spectrum.tmux
@@ -5,5 +5,9 @@ CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 [[ ! $(tmux -V | cut -d' ' -f2) < 2.3 ]] &&
   tmux set-hook -g after-new-session "run-shell 'bash \"$CURRENT_DIR/tmux-session-spectrum.sh\"'" &&
   "$CURRENT_DIR/tmux-session-spectrum.sh"
-tmux set-hook -g session-renamed "run-shell 'bash \"$CURRENT_DIR/tmux-session-spectrum.sh\"'" &&
-"$CURRENT_DIR/tmux-session-spectrum.sh"
+[[ ! $(tmux -V | cut -d' ' -f2) < 2.3 ]] &&
+  tmux set-hook -g session-renamed "run-shell 'bash \"$CURRENT_DIR/tmux-session-spectrum.sh\"'" &&
+  "$CURRENT_DIR/tmux-session-spectrum.sh"
+[[ ! $(tmux -V | cut -d' ' -f2) < 2.3 ]] &&
+  tmux set-hook -g client-session-changed "run-shell 'bash \"$CURRENT_DIR/tmux-session-spectrum.sh\"'" &&
+  "$CURRENT_DIR/tmux-session-spectrum.sh"
