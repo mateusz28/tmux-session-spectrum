734401b8f9565a9ba895c203887aac67b0383566
Mateusz <mateusz28@gmail.com>
2025-07-02 11:41:01 +0200

    color for changing session

--- tmux-session-spectrum.sh
+++ tmux-session-spectrum.sh
@@ -22,7 +22,7 @@ tmux set-hook -t $SESSION_NAME after-new-window[1] \
 
 #tmux nested
 tmux set-hook -t $SESSION_NAME after-new-window[2]\
-  "bind -n M-F11 set -q -t $SESSION_NAME status-bg default
+  "bind -n M-F11 set -q -t $SESSION_NAME status-bg default"
 tmux set-hook -t $SESSION_NAME after-new-window[3]\
   "bind -n M-F12 set -q -t $SESSION_NAME status-bg $STYLE"
 
@@ -31,7 +31,7 @@ tmux bind -n M-F12 set -q -t $SESSION_NAME status-bg $STYLE
 #tmux nested
 ARGS_ENTER="send-keys M-F11 \; set -q status-bg $STYLE \; set-option -g window-active-style fg=default,bg=terminal \; bind-key -n M-h select-pane -L \; bind-key -n M-j select-pane -D \; bind-key -n M-k select-pane -U \; bind-key -n M-l select-pane -R \; set-option -gq prefix M-z"
 
-ARGS_EXIT="send-keys M-F12 \; set -q status-bg $FOREGROUND_COLOR \; set-option -g window-active-style fg=default,bg=default \; unbind-key -n M-h \; unbind-key -n M-j \; unbind-key -n M-k \; unbind-key -n M-l \; set-option -gq prefix M-s"
+ARGS_EXIT="send-keys M-F12 \; set -q status-bg $FOREGROUND_COLOR \; set-option -g window-active-style fg=default,bg=terminal \; unbind-key -n M-h \; unbind-key -n M-j \; unbind-key -n M-k \; unbind-key -n M-l \; set-option -gq prefix M-s"
 
 tmux bind -n M-C-j $ARGS_ENTER
 tmux bind -n M-C-k $ARGS_EXIT
